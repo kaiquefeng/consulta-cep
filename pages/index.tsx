26247c95ef2464c334069b9136cc3b9107d71be4
kaiquemaia <kaiquefeng1@gmail.com>
2021-04-29 15:25:16 -0300

    feat: create search button API

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import styles from '../styles/home.module.scss'
+import api from './services/api'
+
+type FullInfoCepProps = {
+  cep: string,
+  city: string,
+  neighborhood: string,
+  state: string,
+  street: string
+}
+
+
+export default function Home() {
+  const [isCep, setIsCep] = useState('')
+  const [fullInfoCep, setFullInfoCep] = useState<FullInfoCepProps>()
+
+  function SearchCep() {
+    const response = api.get(isCep)
+
+    response.then(result => {
+      setFullInfoCep(result.data)
+    })
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.searchBox}>
+        <h4>Pesquise por um CEP</h4>
+        <input type="number" placeholder="digite um cep" onChange={event => setIsCep(event.target.value)}/>
+        <button onClick={SearchCep}>Pesquisar</button>
+      </div>
+      <div className={styles.card}>
+        { fullInfoCep && (
+          <ul>
+            <li>{fullInfoCep.cep}</li>
+            <li>{fullInfoCep.city}</li>
+            <li>{fullInfoCep.neighborhood}</li>
+            <li>{fullInfoCep.state}</li>
+            <li>{fullInfoCep.street}</li>
+          </ul>
+        )}
+      </div>
+    </div>
+  )
+}
---
+++ pages/services/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'https://brasilapi.com.br/api/cep/v1/' })
+
+export default api
