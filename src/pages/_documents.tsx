9493d63069322da422248f204088104edf1f72cc
kaiquemaia <kaiquefeng1@gmail.com>
2021-04-30 00:45:35 -0300

    fix: new layou and results

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,16 @@
+import Document, { Head, Html } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <title>Consulta CEP</title>
+
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&family=Poppins:wght@500&display=swap" rel="stylesheet"></link>
+        </Head>
+      </Html>
+    )
+  }
+}
