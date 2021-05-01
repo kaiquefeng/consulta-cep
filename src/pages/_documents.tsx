1fabfe16335a2e183ec17f1383ecec2803104d46
kaiquemaia <kaiquefeng1@gmail.com>
2021-05-01 01:40:41 -0300

    fix: add icon and title

--- src/pages/_documents.tsx
+++ src/pages/_documents.tsx
@@ -5,10 +5,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <title>Consulta CEP</title>
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;700&family=Poppins:wght@500&display=swap" rel="stylesheet"></link>
+
+          <link rel="shortcut icon" href="/images/happy.svg" type="image/svg"/>
+
         </Head>
       </Html>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import styles from '../../styles/home.module.scss'
 import { ResultCep } from '../components/ResultCep'
 import { SearchBox } from '../components/SearchBox'
@@ -6,6 +7,9 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Consulta CEP</title>
+      </Head>
       <SearchBox />
       <ResultCep />
     </div>
