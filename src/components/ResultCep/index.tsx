aaa799e39b8f9b52137b4059338dcaa453afd23d
Kaique Feng <kaique.feng@luizalabs.com>
2021-09-22 18:38:09 -0300

    chore: add mask input

--- src/components/ResultCep/index.tsx
+++ src/components/ResultCep/index.tsx
@@ -1,40 +1,60 @@
-import { useSearch } from "../../hooks/useSearchCep"
-import styles from './styles.module.scss'
+import { useSearch } from "../../hooks/useSearchCep";
+import styles from "./styles.module.scss";
 
 export function ResultCep() {
+  const { isSearch, error, fullInfoCep, clearCep } = useSearch();
 
-  const { isSearch, error, fullInfoCep, clearCep } = useSearch()
+  function copyClipboard() {
+    var list = document.querySelector("#list-result").innerHTML;
+    list = list.replace(/\s+<li>/g, "");
+    list = list.replace(/<\/?li>/g, "\r");
+  }
 
   return (
     <div className={styles.result}>
-        { !isSearch && (
-          <div className={styles.noCep}>
-            <img src={!error ? './images/happy.svg' : './images/sad.svg'} alt="Happy"/>
-            <h4>
-              { !error 
-                ? 'Olá, iremos apresentar o endereço de acordo com o CEP que você digitar'
-                : 'Infelizmente não achamos nada com esse CEP'
-              }
-            </h4>
-          </div>
-        )}
-        
-        { isSearch && (
-          <div className={styles.resultSucess}>
-            <img src="./images/sucess.svg" alt="Sucesso"/>
-            <div className={styles.card}>
-              <ul>
-                <li><span>CEP:</span> {fullInfoCep.cep}</li>
-                <li><span>Estado: </span> {fullInfoCep.state}</li>
-                <li><span>Logradouro: </span> {fullInfoCep.street}</li>
-                <li><span>Cidade: </span> {fullInfoCep.city}</li>
-                <li><span>Bairro: </span> {fullInfoCep.neighborhood}</li>
-              </ul>
-            </div>
+      {!isSearch && (
+        <div className={styles.noCep}>
+          <img
+            src={!error ? "./images/happy.svg" : "./images/sad.svg"}
+            alt="Happy"
+          />
+          <h4>
+            {!error
+              ? "Olá, iremos apresentar o endereço de acordo com o CEP que você digitar"
+              : "Infelizmente não achamos nada com esse CEP"}
+          </h4>
+        </div>
+      )}
 
-            <button onClick={clearCep}>Limpar busca</button>
+      {isSearch && (
+        <div className={styles.resultSucess}>
+          <img src="./images/sucess.svg" alt="Sucesso" />
+          <div className={styles.card}>
+            <button className={styles.copyButton}>copy</button>
+            <ul id="list-result">
+              <li>
+                <span>CEP:</span> {fullInfoCep.cep}
+              </li>
+              <li>
+                <span>Estado: </span> {fullInfoCep.state}
+              </li>
+              <li>
+                <span>Logradouro: </span> {fullInfoCep.street}
+              </li>
+              <li>
+                <span>Cidade: </span> {fullInfoCep.city}
+              </li>
+              <li>
+                <span>Bairro: </span> {fullInfoCep.neighborhood}
+              </li>
+            </ul>
           </div>
-        )}
-      </div>
-  )
+
+          <button className={styles.buttonSubmit} onClick={clearCep}>
+            Limpar busca
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -1,20 +1,28 @@
-import { useSearch } from '../../hooks/useSearchCep'
+import { useSearch } from "../../hooks/useSearchCep";
+import InputMask from "react-input-mask";
 
-import { FiSearch } from 'react-icons/fi'
-import styles from './styles.module.scss'
+import { FiSearch } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
 export function SearchBox() {
-  const { setValueSearch, SearchCep } = useSearch()
+  const { setValueSearch, SearchCep } = useSearch();
 
   return (
     <div className={styles.searchBox}>
       <h4>Consultar CEP</h4>
       <div className={styles.search}>
         <div className={styles.inputBlock}>
-          <input type="number" placeholder="Digite um cep" onChange={event => setValueSearch (event.target.value)}/>
+          <InputMask
+            mask="99999-999"
+            maskChar={null}
+            placeholder="Digite um cep"
+            onChange={(event: any) => setValueSearch(event.target.value)}
+          />
         </div>
-        <div className={styles.buttonSearch} onClick={SearchCep}><FiSearch /></div>
+        <button className={styles.buttonSearch} onClick={SearchCep}>
+          <FiSearch />
+        </button>
       </div>
     </div>
-  )
+  );
 }
